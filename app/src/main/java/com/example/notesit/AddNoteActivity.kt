6985d17ca9baeaa2420ececc3902d60c5c826c3f
Serg8787 <tsybulnikserg@gmail.com>
2021-07-25 11:17:51 +0300

    check in addActitvity

--- app/src/main/java/com/example/notesit/AddNoteActivity.kt
+++ app/src/main/java/com/example/notesit/AddNoteActivity.kt
@@ -3,6 +3,7 @@ package com.example.notesit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_note.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -16,15 +17,20 @@ class AddNoteActivity : AppCompatActivity() {
 
 
         btAddNote.setOnClickListener {
-            val title:String = etTitleAdd.text.toString()
-            val description:String = etdescriptionAdd.text.toString()
-            val sdf = SimpleDateFormat("dd/M/yyyy kk:mm",Locale.getDefault())
-            val currentDate = sdf.format(Date())
-            val date:String = currentDate
-            val note:Note = Note(title = title,description = description,date = date,isImpotent = false)
-            noteDatabase.noteDao().insertNote(note)
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if(etTitleAdd.text.toString().isNotEmpty() && etdescriptionAdd.text.toString().isNotEmpty() ) {
+                val title: String = etTitleAdd.text.toString()
+                val description: String = etdescriptionAdd.text.toString()
+                val sdf = SimpleDateFormat("dd/M/yyyy kk:mm", Locale.getDefault())
+                val currentDate = sdf.format(Date())
+                val date: String = currentDate
+                val note: Note =
+                    Note(title = title, description = description, date = date, isImpotent = false)
+                noteDatabase.noteDao().insertNote(note)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this,"Заполните все поля", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
